13159141eda0a4f71a20dc916425db31222f49ba
pengjunzhen <pengjunzhenwork@163.com>
2020-09-13 19:00:36 +0800

    feat: 棋子放在超出X轴边界的地方时，将引发RuntimeException异常

---
+++ src/main/java/org/example/tddjava/chapter3/TicTacToe.java
@@ -0,0 +1,10 @@
+package org.example.tddjava.chapter3;
+
+public class TicTacToe {
+
+    public void play(int x, int y) {
+        if (x < 1 || x > 3) {
+            throw new RuntimeException();
+        }
+    }
+}
---
+++ src/test/java/org/example/tddjava/chapter3/TicTacToeTest.java
@@ -0,0 +1,19 @@
+package org.example.tddjava.chapter3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeTest {
+
+    private TicTacToe ticTacToe;
+    @BeforeEach
+    public final void initEach() {
+        this.ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void should_throw_RuntimeException_when_x_outside_board() {
+        Assertions.assertThrows(RuntimeException.class, () -> ticTacToe.play(5, 1));
+    }
+}
