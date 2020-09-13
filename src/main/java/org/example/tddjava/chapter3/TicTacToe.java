6c27bdd956263e4a253dc277506899ed2429f4b2
pengjunzhen <pengjunzhenwork@163.com>
2020-09-13 20:06:26 +0800

    refactor: 重构斜对角线棋子获胜情况

--- src/main/java/org/example/tddjava/chapter3/TicTacToe.java
+++ src/main/java/org/example/tddjava/chapter3/TicTacToe.java
@@ -20,7 +20,11 @@ public class TicTacToe {
     private boolean isWin() {
 
         int playerTotal = lastPlayer * 3;
+        char diagonal1 = '\0';
+        char diagonal2 = '\0';
         for (int i = 0; i < SIZE; i++) {
+            diagonal1 += board[i][i];
+            diagonal2 += board[i][SIZE - i - 1];
             if (board[0][i] + board[1][i] + board[2][i] == playerTotal) {
                 return true;
             } else if (board[i][0] + board[i][1] + board[i][2] == playerTotal) {
@@ -28,8 +32,7 @@ public class TicTacToe {
             }
         }
 
-        if (board[0][0] + board[1][1] + board[2][2] == playerTotal ||
-                board[0][2] + board[1][1] + board[2][0] == playerTotal) {
+        if (diagonal1 == playerTotal || diagonal2== playerTotal) {
             return true;
         }
         return false;
