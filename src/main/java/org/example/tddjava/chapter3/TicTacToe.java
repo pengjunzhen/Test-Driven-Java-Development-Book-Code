d7f07f1185724ba7ded282241e49eeb49b2fd26c
pengjunzhen <pengjunzhenwork@163.com>
2020-09-13 19:13:40 +0800

    refactor: 重构代码以增强代码可读性

--- src/main/java/org/example/tddjava/chapter3/TicTacToe.java
+++ src/main/java/org/example/tddjava/chapter3/TicTacToe.java
@@ -5,12 +5,19 @@ public class TicTacToe {
     private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
 
     public void play(int x, int y) {
-        if (x < 1 || x > 3) {
-            throw new RuntimeException();
-        } else if (y < 1 || y > 3) {
+        checkAxis(x);
+        checkAxis(y);
+
+        setBox(x, y);
+    }
+
+    private void checkAxis(int axis) {
+        if (axis < 1 || axis > 3) {
             throw new RuntimeException();
         }
+    }
 
+    private void setBox(int x, int y) {
         if (board[x - 1][y - 1] != '\0') {
             throw new RuntimeException();
         } else {
